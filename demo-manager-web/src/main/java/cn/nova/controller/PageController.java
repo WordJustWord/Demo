6d70f33b25f02b5abf99b359f06d52e074a40c8d
CNova <tiandicx@qq.com>
2017-07-12 22:44:38 +0800

    页面初始化

---
+++ demo-manager-web/src/main/java/cn/nova/controller/PageController.java
@@ -0,0 +1,19 @@
+package cn.nova.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/{page}")
+	public String ShowPage(@PathVariable String page) {
+		return page;
+	}
+}
