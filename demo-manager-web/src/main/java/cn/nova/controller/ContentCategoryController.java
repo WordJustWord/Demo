16d243c71f253f06968ac28116c70f86161a9d35
C.Nova <tiandicx@qq.com>
2017-07-23 10:32:15 +0800

    重构content框架

---
+++ demo-manager-web/src/main/java/cn/nova/controller/ContentCategoryController.java
@@ -0,0 +1,23 @@
+package cn.nova.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.nova.pojo.EasyUITreeNode;
+
+@Controller
+@RequestMapping("/content/category")
+public class ContentCategoryController {
+
+//	@Autowired
+//	private 
+	
+//	@RequestMapping("/list")
+//	public List<EasyUITreeNode> InitCategory(@RequestParam(value="id",defaultValue="0") Long parentId){
+//		List<EasyUITreeNode> treeNodes=
+//	}
+}
