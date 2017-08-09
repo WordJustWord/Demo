439670b01ffba515e394e7fa7707183134af1e93
cnova <tiandicx@qq.com>
2017-08-09 23:14:50 +0800

    上传图片成功

--- demo-manager-web/src/main/java/cn/nova/controller/PicUploadController.java
+++ demo-manager-web/src/main/java/cn/nova/controller/PicUploadController.java
@@ -4,15 +4,20 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import cn.nova.common.utils.FastDFSClient;
 import cn.nova.common.utils.IDUtils;
 
 @Controller
 public class PicUploadController {
+
+	@Value("${IMAGE_SERVER_URL}")
+	private String IMAGE_SERVER_URL;
 
 	@RequestMapping("/pic/upload")
 	@ResponseBody
@@ -20,11 +25,13 @@ public class PicUploadController {
 		Map map = new HashMap<>();
 		try {
 			String filename = uploadFile.getOriginalFilename();
-			String extName = filename.substring(filename.lastIndexOf("."));
-			String realName = IDUtils.genImageName() + extName;
-			uploadFile.transferTo(new File("E:\\Develop\\Upload\\Images\\" + realName));
+			String extName = filename.substring(filename.lastIndexOf(".") + 1);
+			// 上传文件到图片
+			FastDFSClient dfsClient = new FastDFSClient("classpath:configers/client.conf");
+			String url = dfsClient.uploadFile(filename.getBytes(), extName);
+			url = IMAGE_SERVER_URL + url;
 			map.put("error", 0);
-			map.put("url", "E:\\Develop\\Upload\\Images\\" + realName);
+			map.put("url", url);
 		} catch (Exception e) {
 			e.printStackTrace();
 			map.put("error", 1);
